de640639267cc8359001cc90c68f2bcfd10bc09b
SporyTike <45672834+SporyTike@users.noreply.github.com>
2024-06-17 22:56:19 +0200

    Remove unnecessary folder layer

---
+++ scripts/init.lua
@@ -0,0 +1,40 @@
+-- entry point for all lua code of the pack
+-- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
+ENABLE_DEBUG_LOG = false
+-- get current variant
+local variant = Tracker.ActiveVariantUID
+
+print("-- ANIMAL WELL Tracker --")
+print("Loaded variant: ", variant)
+if ENABLE_DEBUG_LOG then
+    print("Debug logging is enabled!")
+end
+
+-- Utility Script for helper functions etc.
+ScriptHost:LoadScript("scripts/utils.lua")
+
+-- Logic
+ScriptHost:LoadScript("scripts/logic/logic.lua")
+
+-- Items
+Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/settings.json")
+
+if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
+    -- Maps
+    Tracker:AddMaps("maps/maps.json")    
+    -- Locations
+    Tracker:AddLocations("locations/locations.json")
+end
+
+-- Layout
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/settings.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+
+-- AutoTracking for Poptracker
+if PopVersion and PopVersion >= "0.18.0" then
+	ScriptHost:LoadScript("scripts/ap_helper.lua")
+    ScriptHost:LoadScript("scripts/autotracking.lua")
+end
