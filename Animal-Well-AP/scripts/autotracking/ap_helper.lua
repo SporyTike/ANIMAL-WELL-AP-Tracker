ca33811c96f1a1ea6f215df3e57cc242414adc34
SporyTike <45672834+SporyTike@users.noreply.github.com>
2024-06-17 20:01:15 +0200

    Add files via upload

---
+++ Animal-Well-AP/scripts/autotracking/ap_helper.lua
@@ -0,0 +1,45 @@
+MAP_TOGGLE={[0]=0,[1]=1}
+MAP_TRIPLE={[0]=1,[1]=1,[2]=2}
+
+SLOT_CODES = {
+	disc_hopping={
+		code="option_disc_hopping", 
+		mapping=MAP_TRIPLE
+	},
+	weird_tricks={
+		code="option_weird_tricks", 
+		mapping=MAP_TOGGLE
+	},
+	goal={
+		code="option_goal", 
+		mapping=MAP_TOGGLE
+	},
+	bunnies_as_checks={
+		code="option_bunnies_as_checks", 
+		mapping=MAP_TRIPLE
+	},
+	matchbox={
+		code="option_matchbox", 
+		mapping=MAP_TOGGLE
+	},
+	key_ring={
+		code="option_key_ring", 
+		mapping=MAP_TOGGLE
+	},
+	bubble_jumping={
+		code="option_bubble_jumping", 
+		mapping=MAP_TRIPLE
+	},
+	candle_checks={
+		code="option_candle_checks", 
+		mapping=MAP_TOGGLE
+	},
+	wheel_hopping={
+		code="option_wheel_hopping", 
+		mapping=MAP_TOGGLE
+	},
+	-- eggs_needed={
+		-- code="option_eggs_needed", 
+		-- mapping=MAP_TRIPLE
+	-- }
+}
---
+++ Animal-Well-AP/scripts/autotracking/item_mapping.lua
@@ -0,0 +1,111 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "consumable" but feel free to expand for your needs!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+ITEM_MAPPING = {
+    [00000] = {"bwand", "progressive"},
+    [00001] = {"disc", "progressive"},
+    [00002] = {"yoyo", "toggle"},
+    [00003] = {"slink", "toggle"},
+	[00004] = {"animalflute", "toggle"},
+	[00005] = {"top", "toggle"},
+	[00006] = {"lantern", "toggle"},
+	[00007] = {"uvlantern", "toggle"},
+	[00008] = {"bball", "toggle"},
+	[00009] = {"remote", "toggle"},
+	[00010] = {"wheel", "toggle"},
+	[00011] = {"firecrackers", "progressive"},
+	
+	[00012] = {"mdisc", "progressive"},
+	[00013] = {"fpack", "progressive"},
+	
+	[00014] = {"match", "consumable"},
+	[00015] = {"matchbox", "consumable"},
+	
+	[00016] = {"key", "consumable"},
+	[00017] = {"keyring", "consumable"},
+	[00018] = {"housekey", "toggle"},
+	[00019] = {"officekey", "toggle"},
+	
+	[00020] = {"emedal", "toggle"},
+	[00021] = {"smedal", "toggle"},
+	[00022] = {"kshard", "progressive"},
+	
+	[00023] = {"blue_flame", "toggle"},
+	[00024] = {"green_flame", "toggle"},
+	[00025] = {"violet_flame", "toggle"},
+	[00026] = {"pink_flame", "toggle"},
+	
+	[00027] = {"egg", "consumable"}, --Reference Egg
+	[00028] = {"egg", "consumable"}, --Brown Egg
+	[00029] = {"egg", "consumable"}, --Raw Egg
+	[00030] = {"egg", "consumable"}, --Pickled Egg
+	[00031] = {"egg", "consumable"}, --Big Egg
+	[00032] = {"egg", "consumable"}, --Swan Egg
+	[00033] = {"egg", "consumable"}, --Forbidden Egg
+	[00034] = {"egg", "consumable"}, --Shadow Egg
+	[00035] = {"egg", "consumable"}, --Vanity Egg
+	[00036] = {"egg", "consumable"}, --Service As An Egg
+	
+	[00037] = {"egg", "consumable"}, --Depraved Egg
+	[00038] = {"egg", "consumable"}, --Chaos Egg
+	[00039] = {"egg", "consumable"}, --Upside Down Egg
+	[00040] = {"egg", "consumable"}, --Evil Egg
+	[00041] = {"egg", "consumable"}, --Sweet Egg
+	[00042] = {"egg", "consumable"}, --Chocolate Egg
+	[00043] = {"egg", "consumable"}, --Value Egg
+	[00044] = {"egg", "consumable"}, --Plant Egg
+	[00045] = {"egg", "consumable"}, --Red Egg
+	[00046] = {"egg", "consumable"}, --Orange Egg
+	[00047] = {"egg", "consumable"}, --Sour Egg
+	[00048] = {"egg", "consumable"}, --Post Modern Egg
+	
+	[00049] = {"egg", "consumable"}, --Universal Basic Egg
+	[00050] = {"egg", "consumable"}, --Laissez-faire Egg
+	[00051] = {"egg", "consumable"}, --Zen Egg
+	[00052] = {"egg", "consumable"}, --Future Egg
+	[00053] = {"egg", "consumable"}, --Friendship Egg
+	[00054] = {"egg", "consumable"}, --Truth Egg
+	[00055] = {"egg", "consumable"}, --Transcendental Egg
+	[00056] = {"egg", "consumable"}, --Ancient Egg
+	[00057] = {"egg", "consumable"}, --Magic Egg
+	[00058] = {"egg", "consumable"}, --Mystic Egg
+	[00059] = {"egg", "consumable"}, --Holiday Egg
+	[00060] = {"egg", "consumable"}, --Rain Egg
+	[00061] = {"egg", "consumable"}, --Razzle Egg
+	[00062] = {"egg", "consumable"}, --Dazzle Egg
+	
+	[00063] = {"egg", "consumable"}, --Virtual Egg
+	[00064] = {"egg", "consumable"}, --Normal Egg
+	[00065] = {"egg", "consumable"}, --Great Egg
+	[00066] = {"egg", "consumable"}, --Gorgeous Egg
+	[00067] = {"egg", "consumable"}, --Planet Egg
+	[00068] = {"egg", "consumable"}, --Moon Egg
+	[00069] = {"egg", "consumable"}, --Galaxy Egg
+	[00070] = {"egg", "consumable"}, --Sunset Egg
+	[00071] = {"egg", "consumable"}, --Goodnight Egg
+	[00072] = {"egg", "consumable"}, --Dream Egg
+	[00073] = {"egg", "consumable"}, --Travel Egg
+	[00074] = {"egg", "consumable"}, --Promise Egg
+	[00075] = {"egg", "consumable"}, --Ice Egg
+	[00076] = {"egg", "consumable"}, --Fire Egg
+	
+	[00077] = {"egg", "consumable"}, --Bubble Egg
+	[00078] = {"egg", "consumable"}, --Desert Egg
+	[00079] = {"egg", "consumable"}, --Clover Egg
+	[00080] = {"egg", "consumable"}, --Brick Egg
+	[00081] = {"egg", "consumable"}, --Neon Egg
+	[00082] = {"egg", "consumable"}, --Iridescent Egg
+	[00083] = {"egg", "consumable"}, --Rust Egg
+	[00084] = {"egg", "consumable"}, --Scarlet Egg
+	[00085] = {"egg", "consumable"}, --Sapphire Egg
+	[00086] = {"egg", "consumable"}, --Ruby Egg
+	[00087] = {"egg", "consumable"}, --Jade Egg
+	[00088] = {"egg", "consumable"}, --Obsidian Egg
+	[00089] = {"egg", "consumable"}, --Crystal Egg
+	[00090] = {"egg", "consumable"}, --Golden Egg
+	
+	[00091] = {"egg", "consumable"}, --65th Egg
+	
+	[00092] = {}, --Firecracker Refill
+	[00093] = {}, --Big Blue Fruit
+}
